7bc29245aee1fbb5be5e45511574106c5db376c3
Maheshh Thakor <156879292+Maheshh-03@users.noreply.github.com>
2026-03-22 10:36:54 +0530

    Update contactRoutes.js

--- contactRoutes.js
+++ contactRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const rateLimiter = require("../middleware/rateLimiter");
-const { submitContact } = require("../controllers/contactController");
+const { submitContact } = require("contactController.js");
 
 router.post("/", rateLimiter, submitContact);
 
